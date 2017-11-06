a0e92e3250c1b617d780315bac8d39c043a5fc64
Danil <keepfocused@users.noreply.github.com>
2017-11-06 20:06:52 +0300

    Keep working

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -4,29 +4,70 @@
 //
 //  Created by Admin on 06.11.17.
 //  Copyright © 2017 Danil. All rights reserved.
-//
+// √
 
 import Foundation
+
+func changeSign(_ operand: Double) -> Double {
+    return -operand
+}
+
+func multiply (op1: Double,op2: Double) -> Double {
+    return op1 * op2
+}
+
 
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
-    mutating func perfromOperation(_ symbol: String) {
-        switch symbol {
-        case "П":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator
-            {
-                accumulator = sqrt(operand)
-            }
-        default:
-            break
-        }
-        
-        
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
     }
+    
+    private var operations: Dictionary<String,Operation> =
+        [
+            "П" : Operation.constant(Double.pi),
+            "e" : Operation.constant(M_E),
+            "√" : Operation.unaryOperation(sqrt), // sqrt
+            "cos" : Operation.unaryOperation(cos),
+            "±" : Operation.unaryOperation(changeSign),
+            "x" : Operation.binaryOperation(multiply),
+            "=" : Operation.equals
+        ]
+    
+    
+    
+    mutating func perfromOperation(_ symbol: String) {
+        if let operation = operations[symbol]
+        {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                break
+            case .equals:
+                break
+            }
+        }
+    }
+    
+    private struct PendingOperation {
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return firstOperand * secondOperand
+        }
+    }
+    
     
     mutating func setOperand(_ operand: Double) {
         accumulator = operand
